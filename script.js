6823d28e6b93ffd58c148bc13087f5515076bd34
shashank-njr10 <141976194+shashank-njr10@users.noreply.github.com>
2024-08-17 12:08:29 +0530

    updated comment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-//Selecting comments
+//Selecting comments .........
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
